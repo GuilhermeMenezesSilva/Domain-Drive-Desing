fae23961eff7a88ea512179a41bd8c95e4297b94
Guilherme Menezes <107522195+GuilhermeMenezesSilva@users.noreply.github.com>
2022-10-07 17:29:02 -0300

    Classe de teste do programa

---
+++ PrimeiroPrograma/Teste.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Teste{
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+
+        // OU 
+
+        JOptionPane.showMessageDialog(null, "Olá Mundo!");
+    }
+}
