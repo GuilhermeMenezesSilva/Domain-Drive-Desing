7da971fb02cbb4044a9d2ad41f254b02a36c65db
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2022-10-25 18:50:23 -0300

    Classe pai do programa

---
+++ POO/Polimorfismo/rh/src/Funcionario.java
@@ -0,0 +1,37 @@
+public abstract class Funcionario {
+
+    private String nome;
+    private int codigo;
+    private double salario;
+
+    public Funcionario(String nome, int codigo, double salario) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+}
