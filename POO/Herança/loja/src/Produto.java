caf6aca17ee1d51624b1fa8362dd6da899940c87
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2022-10-25 18:23:16 -0300

    Classe principal do programa

---
+++ POO/Herança/loja/src/Produto.java
@@ -0,0 +1,26 @@
+public class Produto {
+    protected String descricao;
+    private double preco;
+
+    public Produto(String descricao, double preco) {
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+}
